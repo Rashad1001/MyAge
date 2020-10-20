18f50b079c0a132fcef8a937b827c9535688195c
Rashad1001 <rashad.edwards@patelhs.org>
2020-10-20 13:39:43 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+class Main{
+    public static void MyAge(int age){
+        if (age == 16){
+            System.out.println("You can't buy GTA V");
+        }
+        else{
+            System.out.println("You can buy GTA V");
+        }
+    }
+    public static void main(String args[]){
+        MyAge(16);
+    }
+}
